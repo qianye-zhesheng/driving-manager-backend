99ad9e8dffb23323e5161fdb0061d444288fe686
tetsuo <info@asano-tetsuo.com>
2025-05-27 15:42:59 +0900

    prettier初回一括適用

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default [
   defineConfig([
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
+    { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.node } },
     tseslint.configs.recommended,
-    globalIgnores([".aws-sam/"]),
+    globalIgnores(['.aws-sam/']),
   ]),
-  eslintConfigPrettier
-];
+  eslintConfigPrettier,
+]
