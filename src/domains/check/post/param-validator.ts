6a31e372a834e15c3e4ffa6618bcde8ea70bff75
tetsuo <info@asano-tetsuo.com>
2025-06-04 13:43:57 +0900

    運行前チェック保存 リファクタリング

--- src/domains/check/post/param-validator.ts
+++ src/domains/check/post/param-validator.ts
@@ -1,21 +1,20 @@
 import { EmptyBodyValidator } from './empty-body-validator'
 import { ValidationResult } from './validation-result'
-import { AnswerParam } from './answer-param'
 import { ParamDetailValidator } from './param-detail-validator'
 
 export class ParamValidator {
-  private constructor(private readonly bodyJSON: string | null) {}
+  private constructor(private readonly requestBody: string | null) {}
 
-  public static of(bodyJSON: string | null): ParamValidator {
-    return new ParamValidator(bodyJSON)
+  public static of(requestBody: string | null): ParamValidator {
+    return new ParamValidator(requestBody)
   }
 
   public validate(): ValidationResult {
-    const emptyBodyValidation = EmptyBodyValidator.validate(this.bodyJSON)
+    const emptyBodyValidation = EmptyBodyValidator.validate(this.requestBody)
     if (emptyBodyValidation.isInvalid()) {
       return emptyBodyValidation
     }
 
-    return ParamDetailValidator.validate(this.bodyJSON as string)
+    return ParamDetailValidator.validate(this.requestBody as string)
   }
 }
