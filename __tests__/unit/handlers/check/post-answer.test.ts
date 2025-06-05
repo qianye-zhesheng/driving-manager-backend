42e0e7cb251e3b19bd6f41c923c077f6fcdfaabd
tetsuo <info@asano-tetsuo.com>
2025-06-05 14:22:55 +0900

    運行前チェック保存 handlerの単体テスト

---
+++ __tests__/unit/handlers/check/post-answer.test.ts
@@ -0,0 +1,63 @@
+import { postAnswerHandler } from '../../../../src/handlers/check/post-answer'
+import { ParamValidator } from '../../../../src/domains/check/post/param-validator'
+import { ParamParser } from '../../../../src/domains/check/post/param-parser'
+import { Response } from '../../../../src/domains/check/post/response'
+import { AnswerRepository } from '../../../../src/domains/check/post/answer-repository'
+import { ValidationResult } from '../../../../src/domains/check/post/validation-result'
+import dayjs from 'dayjs'
+import event from '../../../../events/check/event-put-answer.json'
+import { AnswerParam } from '../../../../src/domains/check/post/answer-param'
+import { APIGatewayProxyEvent } from 'aws-lambda'
+
+describe('postAnswerHandlerのテスト', () => {
+  const anserParam: AnswerParam = JSON.parse(event.body) as AnswerParam
+  const datetime: string = '2025-01-01T00:00:00+09:00'
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    jest.spyOn(ParamParser.prototype, 'parse').mockImplementation(() => {
+      return anserParam
+    })
+
+    jest.spyOn(AnswerRepository.prototype, 'saveAnswer').mockImplementation(() => {
+      return Promise.resolve(Response.of200(anserParam, datetime))
+    })
+  })
+
+  test('有効なparamなら200を返すこと', async () => {
+    jest.spyOn(ParamValidator.prototype, 'validate').mockImplementation(() => {
+      return ValidationResult.valid()
+    })
+
+    const result = await postAnswerHandler(event as APIGatewayProxyEvent)
+
+    expect(result.statusCode).toBe(200)
+    expect(result.body).toBe(
+      JSON.stringify({
+        userId: anserParam.userId,
+        dateTime: datetime,
+        imSafeAnswer: anserParam.imSafeAnswer,
+        weatherAnswer: anserParam.weatherAnswer,
+        judgement: anserParam.judgement,
+      }),
+    )
+
+    expect(ParamValidator.prototype.validate).toHaveBeenCalledTimes(1)
+    expect(AnswerRepository.prototype.saveAnswer).toHaveBeenCalledTimes(1)
+  })
+
+  test('無効なparamなら400を返すこと', async () => {
+    jest.spyOn(ParamValidator.prototype, 'validate').mockImplementation(() => {
+      return ValidationResult.invalid('Invalid parameters')
+    })
+
+    const result = await postAnswerHandler({ httpMethod: 'POST', body: '' } as APIGatewayProxyEvent)
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toBe(JSON.stringify({ message: 'Invalid parameters' }))
+
+    expect(ParamValidator.prototype.validate).toHaveBeenCalledTimes(1)
+    expect(AnswerRepository.prototype.saveAnswer).toHaveBeenCalledTimes(0)
+  })
+})
