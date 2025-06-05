5a1b6299d55c4dfbebbb629895eee770c09da66c
tetsuo <info@asano-tetsuo.com>
2025-06-05 14:36:29 +0900

    運行前チェック保存 正常系のみ仮実装

---
+++ src/domains/check/interfaces.ts
@@ -0,0 +1,15 @@
+export interface ImSafeAnswer {
+  illness: number
+  medication: number
+  stress: number
+  alcohol: number
+  fatigue: number
+  emotion: number
+}
+
+export interface WeatherAnswer {
+  wetRoad: number
+  visibility: number
+  icyRoad: number
+  snow: number
+}
