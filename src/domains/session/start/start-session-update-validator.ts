9f3e7dfaafd4a64819d31a8c6ec798f17d07f604
tetsuo <info@asano-tetsuo.com>
2025-06-23 11:15:29 +0900

    運行開始 update時の開始・終了のバリデーション

---
+++ src/domains/session/start/start-session-update-validator.ts
@@ -0,0 +1,34 @@
+import { DrivingSession } from '../driving-session'
+import { ValidationResult } from '../validation-result'
+
+export class StartSessionUpdateValidator {
+  public constructor(
+    private readonly newDrivingSession: DrivingSession,
+    private readonly existingDrivingSession: DrivingSession,
+  ) {}
+
+  public validate(): ValidationResult {
+    if (this.sessionHasNotFinished()) {
+      //まだ運行が終了していないなら、開始終了の異常値チェック不要
+      return ValidationResult.valid()
+    }
+
+    if (this.hasSmarlerEndThanStart()) {
+      return ValidationResult.invalid(
+        '開始メーター値は、終了メーター値よりも小さい値を指定してください',
+      )
+    }
+
+    return ValidationResult.valid()
+  }
+
+  private sessionHasNotFinished(): boolean {
+    return (
+      !this.existingDrivingSession.finished || this.existingDrivingSession.endOdometer == undefined
+    )
+  }
+
+  private hasSmarlerEndThanStart(): boolean {
+    return this.existingDrivingSession.endOdometer < this.newDrivingSession.startOdometer
+  }
+}
