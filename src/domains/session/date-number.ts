2428f3cbb7ed53287009a2b357faa182dae053ff
tetsuo <info@asano-tetsuo.com>
2025-12-11 17:22:22 +0900

    DateNumberリファクタリング

--- src/domains/session/date-number.ts
+++ src/domains/session/date-number.ts
@@ -9,21 +9,26 @@ export class DateNumber {
   private static readonly YEAR_DIGITS: number = 10000
   private static readonly MONTH_DIGITS: number = 100
 
-  private constructor(private readonly dateString: string) {
-    if (!dayjs(dateString, 'YYYY-MM-DD', true).isValid()) {
-      throw new Error(`dateString is not the format of ${DateNumber.FORMAT}`)
-    }
-  }
+  private constructor(
+    private readonly year: number,
+    private readonly month: number,
+    private readonly day: number,
+  ) {}
 
   public static of(dateString: string): DateNumber {
-    return new DateNumber(dateString)
-  }
+    const date = dayjs(dateString, DateNumber.FORMAT, true)
 
-  public get(): number {
-    const date = dayjs(this.dateString, DateNumber.FORMAT, true)
+    if (!date.isValid()) {
+      throw new Error(`dateString is not the format of ${DateNumber.FORMAT}`)
+    }
+
     const year: number = date.year()
     const month: number = date.month() + 1
     const day: number = date.date()
-    return year * DateNumber.YEAR_DIGITS + month * DateNumber.MONTH_DIGITS + day
+    return new DateNumber(year, month, day)
+  }
+
+  public get(): number {
+    return this.year * DateNumber.YEAR_DIGITS + this.month * DateNumber.MONTH_DIGITS + this.day
   }
 }
