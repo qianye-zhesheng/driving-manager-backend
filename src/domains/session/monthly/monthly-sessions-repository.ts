67a477ea2abec969413b9ad7ad0e9b9a28366e3b
tetsuo <info@asano-tetsuo.com>
2025-12-12 12:21:18 +0900

    前月最新記録の取得（未テスト）

--- src/domains/session/monthly/monthly-sessions-repository.ts
+++ src/domains/session/monthly/monthly-sessions-repository.ts
@@ -2,6 +2,7 @@ import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb'
 import { DrivingSession } from '../driving-session'
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { YearMonthQuery } from './year-month-query'
+import { FindResult } from './find-result'
 
 export class MonthlySessionsRepository {
   private readonly ddbDocClient: DynamoDBDocumentClient
@@ -30,6 +31,30 @@ export class MonthlySessionsRepository {
     )
 
     return drivingSessions.Items.map((item) => this.toEntity(item))
+  }
+
+  public async findPreviousMonthLatestSessionIfExists(
+    userId: string,
+    currentYearMonth: YearMonthQuery,
+  ): Promise<FindResult> {
+    const previousSession = await this.ddbDocClient.send(
+      new QueryCommand({
+        TableName: this.tableName,
+        KeyConditionExpression: 'user_id = :userId AND date_number < :dateNumber',
+        ExpressionAttributeValues: {
+          ':userId': userId,
+          ':dateNumber': currentYearMonth.from().get(),
+        },
+        ScanIndexForward: false,
+        Limit: 1,
+      }),
+    )
+
+    if (previousSession.Count == 0) {
+      return FindResult.ofEmpty()
+    }
+
+    return FindResult.of(this.toEntity(previousSession.Items[0]))
   }
 
   private toEntity(item: Record<string, any>): DrivingSession {
