5d6d04b68b530d2bc477557bca15a3ed1d2bdefe
tetsuo <info@asano-tetsuo.com>
2025-12-17 14:22:03 +0900

    ダッシュボードAPI リファクタリング

--- src/handlers/dashboard/get-current-info.ts
+++ src/handlers/dashboard/get-current-info.ts
@@ -6,7 +6,7 @@ import { DashboardRepository } from '../../domains/dashboard/dashboard-repositor
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
-import { CurrentInfo, CurrentInfoFactory } from '../../domains/dashboard/current-info'
+import { CurrentInfoFactory } from '../../domains/dashboard/current-info'
 
 const CHECK_ANSWERS_TABLE: string = process.env.CHECK_ANSWERS_TABLE as string
 const DRIVING_SESSIONS_TABLE: string = process.env.DRIVING_SESSIONS_TABLE as string
@@ -33,12 +33,12 @@ export const getCurrentInfoHandler = async (
   }
 
   const userId = authInfo.getUserId()
-  const date = dayjs().tz(TIMEZONE).format('YYYY-MM-DD')
+  const today = dayjs().tz(TIMEZONE).format('YYYY-MM-DD')
 
   let response: Response
 
   try {
-    const checkResult = await repository.findLatestCheckAnswer(userId, date)
+    const checkResult = await repository.findLatestCheckAnswer(userId, today)
     const activeSession = await repository.findActiveDrivingSession(userId)
 
     const currentInfo = CurrentInfoFactory.create(checkResult, activeSession)
